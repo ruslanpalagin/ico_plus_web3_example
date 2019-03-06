3bba6435587a470e73ab054a8bc3971cbca401ca
Ruslan Palagin <ruslan.palagin.ck@gmail.com>
2019-03-06 21:38:58 +0200

    init

---
+++ test/TokenTest.js
@@ -0,0 +1,28 @@
+const BAToken = artifacts.require("../contracts/BAToken.sol");
+
+contract('Fight', function (accounts) {
+    
+    var appOwner = accounts[0];
+    var user = accounts[1];
+
+    let tokenAddress;
+
+    let comment = "London is the capital of GB";
+    let amount = 100;
+
+    beforeEach("init", async () => {
+        instanceOfBAToken = await BAToken.new(
+            comment,
+            comment,
+            1000,
+            {from: appOwner}
+        );
+    
+        tokenAddress = instanceOfBAToken.address;
+    });
+
+    it("tests process", async () => {
+        web3.eth.sendTransaction({to:tokenAddress, from:user, value:web3.toWei("1", "ether")}, {from: user})
+        assert.equal(await instanceOfBAToken.balanceOf(user).toNumber(), 1000, "update");
+    });
+});
